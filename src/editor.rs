ce57fe62261cd12bdf3e9cfbe7fc318d79170554
Luc Street <lucis-fluxum@users.noreply.github.com>
2018-01-18 13:22:07 -0800

    Add convenience functions and constructor to Editor
    - Organize imports
    - Rename key handler functions

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,28 @@
+use std::io::{stdout, Stdout, Write};
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::clear;
+use termion::cursor::Goto;
+
 use mode::Mode;
-use termion::raw::RawTerminal;
-use std::io::Stdout;
 
 pub struct Editor {
     pub mode: Mode,
     pub output: RawTerminal<Stdout>,
+}
+
+impl Editor {
+    pub fn new() -> Editor {
+        Editor {
+            mode: Mode::Normal,
+            output: stdout().into_raw_mode().unwrap(),
+        }
+    }
+
+    pub fn clear(&mut self) {
+        write!(self.output, "{}", clear::All).unwrap();
+    }
+
+    pub fn goto(&mut self, x: u16, y: u16) {
+        write!(self.output, "{}", Goto(x, y)).unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,32 @@ mod mode;
 mod editor;
 mod handler;
 
+use std::io::{stdin, Write};
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use termion::cursor::*;
-use std::io::{Write, stdout, stdin};
 
-use mode::Mode;
 use editor::Editor;
+use mode::Mode;
 
 fn main() {
-    let mut ed = Editor { 
-        mode: Mode::Normal, 
-        output: stdout().into_raw_mode().unwrap(),
-    };
-
-    write!(ed.output, "{}{}", termion::clear::All, Goto(1, 1)) .unwrap();
+    let mut ed = Editor::new();
+    ed.clear();
+    ed.goto(1, 1);
     ed.output.flush().unwrap();
 
     for c in stdin().keys() {
         let c = c.unwrap();
         match ed.mode {
-            Mode::Normal => handler::handle_key_for_normal(&c, &mut ed),
-            Mode::Insert => handler::handle_key_for_insert(&c, &mut ed)
+            Mode::Normal => handler::normal_mode_key(&c, &mut ed),
+            Mode::Insert => handler::insert_mode_key(&c, &mut ed),
         }
-        handler::handle_key_for_all(&c, &mut ed);
-        if c == Key::Ctrl('c') { break; }
+        handler::any_mode_key(&c, &mut ed);
+        if c == Key::Ctrl('c') {
+            break;
+        }
         ed.output.flush().unwrap();
     }
 
-    write!(ed.output, "{}{}", termion::clear::All, Goto(1, 1)).unwrap();
+    ed.clear();
+    ed.goto(1, 1);
 }
