c292e9f68e358a401da90c4030b9295f1d312a84
Luc Street <lucis-fluxum@users.noreply.github.com>
2018-02-02 13:31:20 -0800

    Add saving capability to Editor

--- src/action/filesystem.rs
+++ src/action/filesystem.rs
@@ -1,15 +1,21 @@
-use std::io::BufReader;
-use std::io::BufRead;
+use std::io::prelude::*;
+use std::io::{BufReader, LineWriter};
 use std::fs::File;
 
 use editor::Editor;
 
 impl Editor {
     pub fn load(&mut self, filename: &str) {
+        // TODO: Persist the reader within an editor, only read when needed
         let reader = BufReader::new(File::open(filename).unwrap());
         self.buffer = reader.lines().map(|line| line.unwrap()).collect();
         self.refresh_all();
     }
 
-    pub fn save(&self) {}
+    pub fn save(&self, filename: &str) {
+        let mut writer = LineWriter::new(File::create(filename).unwrap());
+        for line in self.buffer.iter() {
+            write!(writer, "{}\n", line).unwrap();
+        }
+    }
 }
