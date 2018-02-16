05664efc947ff8b492f2ebc8fbe90ab18e8ca0c7
Luc Street <lucis-fluxum@users.noreply.github.com>
2018-02-15 21:29:28 -0800

    Add jumping to next/previous lines when moving left or right

--- src/action/movement.rs
+++ src/action/movement.rs
@@ -1,9 +1,10 @@
 use termion::cursor::*;
 
 use editor::Editor;
+use editor::Mode;
 
 // TODO: Scrolling when traveling up or down
-// TODO: Jumping to next/previous lines when moving right or left
+// TODO: Jumping back to end of line when moving up/down from longer line
 impl Editor {
     pub fn pos(&mut self) -> (usize, usize) {
         // TODO: Use cursor_pos function once
@@ -24,16 +25,43 @@ impl Editor {
         // Don't move past the left edge of the terminal
         if n <= x {
             self.goto(x - n, y);
-        } else {
-            // Move to previous line if possible
+        } else if y > 0 {
+            // Normal mode: last char of previous line
+            // Insert mode: one beyond the last char
+            let end = match self.mode {
+                Mode::Normal => self.buffer[y - 1].len() - 1,
+                Mode::Insert => self.buffer[y - 1].len(),
+            };
+
+            // Move to end of previous line instead
+            self.goto(end, y - 1);
         }
     }
 
     pub fn right(&mut self, n: usize) {
         let (x, y) = self.pos();
-        // TODO: Depending on mode, do/don't move past the end of the line
-        // Move to next line if possible
-        self.goto(x + n, y);
+        let current_line_len = self.buffer[y].len();
+
+        // Depending on mode, do or don't move past the end of the line
+        let regular_move = match self.mode {
+            Mode::Normal => x + n < current_line_len,
+            Mode::Insert => x + n <= current_line_len,
+        };
+
+        if regular_move {
+            self.goto(x + n, y);
+        } else if y + 1 < self.buffer.len() {
+            // Next line exists, calculate leftover movement
+            let leftover = match self.mode {
+                Mode::Normal => n - (current_line_len - x),
+                Mode::Insert => n - (current_line_len - x) - 1,
+            };
+
+            // Skip to next line and move leftover amount
+            // This also skips to the line after, if needed.
+            self.goto(0, y + 1);
+            self.right(leftover);
+        }
     }
 
     pub fn up(&mut self, n: usize) {
