f80f27a86bfc0d0823cd625159b8e49c044e26a1
Luc Street <lucis-fluxum@users.noreply.github.com>
2018-02-15 22:35:31 -0800

    Add docs for filesystem module

--- src/action/filesystem.rs
+++ src/action/filesystem.rs
@@ -5,6 +5,8 @@ use std::fs::File;
 use editor::Editor;
 
 impl Editor {
+    /// Load the content of a file into the buffer, overwriting the current
+    /// contents of the buffer.
     pub fn load(&mut self, filename: &str) {
         // TODO: Persist the reader within an editor, only read when needed
         let reader = BufReader::new(File::open(filename).unwrap());
@@ -12,6 +14,8 @@ impl Editor {
         self.refresh_all();
     }
 
+    /// Save the contents of the buffer to a file, truncating the file if it
+    /// exists.
     pub fn save(&self, filename: &str) {
         let mut writer = LineWriter::new(File::create(filename).unwrap());
         for line in self.buffer.iter() {
