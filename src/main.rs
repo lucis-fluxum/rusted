723f2780f20a0938a6a6fd805c82819dabaecb7f
Luc Street <lucis-fluxum@users.noreply.github.com>
2018-02-18 09:34:57 -0800

    Enable loading/saving via command line argument

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,18 @@ mod listen;
 use editor::Editor;
 
 fn main() {
+    let filename = std::env::args().nth(1).unwrap_or(String::from(""));
     let mut editor = Editor::new();
+
+    if !filename.is_empty() {
+        editor.load(&filename);
+    }
+
     editor.respond_to_keys();
+
+    if !filename.is_empty() {
+        editor.save(&filename);
+    } else {
+        // TODO: Prompt for filename
+    }
 }
